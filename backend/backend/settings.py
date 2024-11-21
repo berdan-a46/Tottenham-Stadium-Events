aa939b7cf03923cf89ca266e22de6309373c357e
Berdan Altinkaya <ec21378@qmul.ac.uk>
2024-11-21 16:13:31 +0000

    Editing CORS allowed origins

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -56,6 +56,7 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     'http://192.168.56.1:3000',
+    'https://tottenham-stadium-events-website.onrender.com/',
 ]
 
 ROOT_URLCONF = 'backend.urls'
